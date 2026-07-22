2d340bcb6678efd4626faeafecaa62e3f538193e
unknown <your.email@example.com>
2026-07-22 20:33:15 +0530

    Configure WhiteNoise and ALLOWED_HOSTS for Render

--- asalps_core/settings.py
+++ asalps_core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4rb$0xg-u3$s52e^k8v3c5g%t)1je0!$6)c7z=6g@4zs6%cc=u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Added for production static files
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
